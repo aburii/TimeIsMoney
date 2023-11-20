30f8ba6fd10d4fa75d02de66487f66d484cc399c
kieran <kieran.boudouin@epitech.eu>
2023-11-20 09:49:01 +0100

    [Feature/DefaultLayout] Added scss classes for default layout

--- apps/front/components/AppFooter.vue
+++ apps/front/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer footer-center p-10 bg-base-200">
+  <footer class="footer footer-center p-10 bg-base-200 bottom-0">
     <aside>
       <p class="font-bold">TheCountOfMoney</p>
       <p>Copyright © 2023 - All right reserved</p>
