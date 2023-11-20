d155a36e664c294fc4f070b9c08041f6d6411cbc
Meymoun Boualaoui <boualaoui.meymoun.icn@gmail.com>
2023-11-20 14:15:45 +0100

    (components Home) update

--- apps/front/components/Home/HeroSection.vue
+++ apps/front/components/Home/HeroSection.vue
@@ -1,3 +1,38 @@
 <template>
-<p>hero</p>
+  <section
+    class="flex flex-col-reverse p-4 md:p-8 lg:px-16 justify-between items-center lg:items-stretch lg:flex-row gap-20"
+  >
+    <div class="flex flex-col items-start gap-4 lg:gap-8 lg:w-2/3 xl:w-3/5">
+      <h2 class="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-2">
+        Révolutionnez Votre Expérience de la Cryptomonnaie avec Count of Money
+      </h2>
+      <p
+        class="text-sm md:text-base lg:text-lg xl:text-xl text-justify mb-4 lg:flex-1"
+      >
+        Découvrez Count of Money, votre hub essentiel pour la cryptomonnaie.
+        Suivez, analysez et gérez vos actifs facilement avec nos outils avancés
+        et mises à jour instantanées. Rejoignez-nous et prenez le contrôle de
+        votre avenir financier dès aujourd'hui !
+      </p>
+      <div class="flex flex-col md:flex-row items-center gap-4 lg:gap-8">
+        <button class="bg-blue-500 text-white py-2 px-4 rounded-md">
+          Explorer plus
+        </button>
+        <button class="bg-green-500 text-white py-2 px-4 rounded-md">
+          Commencer
+        </button>
+      </div>
+    </div>
+    <div class="img-container">
+      <img
+        src="@/assets/img/logo.png"
+        class="w-full"
+        alt="Logo Count of Money"
+      />
+    </div>
+  </section>
 </template>
+
+<style scoped>
+
+</style>
