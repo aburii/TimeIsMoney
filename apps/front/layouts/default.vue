b34651bca3a13f467f8fb78b88d10aab8cd51d21
Meymoun Boualaoui <boualaoui.meymoun.icn@gmail.com>
2023-11-20 12:51:03 +0100

    prettier

--- apps/front/layouts/default.vue
+++ apps/front/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="layout-container">
     <AppHeader />
-    <main class="main-content">
+    <main class="main-content text-white px-12">
       <slot />
     </main>
   </div>
