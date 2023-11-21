ad5626f71565306c52e0ad3a8bd1de70cb237a02
Meymoun Boualaoui <boualaoui.meymoun.icn@gmail.com>
2023-11-21 15:24:15 +0100

    (components) reusable components

---
+++ apps/front/components/Home/Custom/BigBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    :class="`rounded-md bg-gradient-to-b ${color} p-1 min-w-[385px] h-[300px]`"
+  >
+    <div
+      class="flex flex-col w-450 h-full flex-col justify-center items-center gap-2 bg-[#1D232A] p-4 text-justify"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps(["color"]);
+</script>
---
+++ apps/front/components/Home/Custom/Box.vue
@@ -0,0 +1,11 @@
+<template>
+	<div :class="`rounded-md bg-gradient-to-b ${color} p-1 min-w-[385px]`">
+		<div class="flex flex-col w-450 h-180 flex-col justify-center items-center gap-2 bg-[#1D232A] p-4 text-justify">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script setup>
+defineProps(['color']);
+</script>
