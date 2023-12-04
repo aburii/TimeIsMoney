a99cf82290af9acf85e2fc33407b694941fafd27
Meymoun Boualaoui <boualaoui.meymoun.icn@gmail.com>
2023-12-04 11:21:08 +0100

    (components) home box take body bg-color

--- apps/front/components/Home/Custom/Box.vue
+++ apps/front/components/Home/Custom/Box.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="`rounded-md bg-gradient-to-b ${color} p-1 min-w-[260px]`">
     <div
-      class="flex flex-col w-450 h-180 flex-col justify-center items-center gap-2 bg-[#1D232A] p-4 text-justify"
+      class="bg-base-100 flex flex-col w-450 h-180 flex-col justify-center items-center gap-2 p-4 text-justify"
     >
       <slot />
     </div>
