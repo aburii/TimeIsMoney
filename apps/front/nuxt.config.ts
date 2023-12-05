065edb2386aea908eb6f6f54ed68d1060f87e69b
Meymoun Boualaoui <boualaoui.meymoun.icn@gmail.com>
2023-12-05 12:42:42 +0100

    (package) add @nuxtjs/svg-sprite

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "The Count of Money - PAR4",
+    },
+  },
+
   devtools: { enabled: true },
   ssr: false,
 
-  modules: ["@timeismoney/ui-components/nuxt", "@vueuse/nuxt", "@pinia/nuxt"],
+  modules: ["@timeismoney/ui-components/nuxt", "@vueuse/nuxt", "@pinia/nuxt", "@nuxtjs/svg-sprite"],
 
   css: ["@/assets/scss/main.scss"],
 
