a5631dc0e24ce6549a773e07a2bc2aa237ac5840
Meymoun Boualaoui <boualaoui.meymoun.icn@gmail.com>
2023-12-05 12:48:47 +0100

    refactor(components): improve design w/ daisyUI

--- apps/front/components/Home/DashBoardPreviewSection.vue
+++ apps/front/components/Home/DashBoardPreviewSection.vue
@@ -1,12 +1,18 @@
 <template>
   <section
-    class="flex flex-col items-center justify-center gap-6 md:gap-10 py-10 md:py-10"
+    class="bg-secondary text-secondary-content flex flex-col items-center justify-center gap-6 md:gap-10 py-10 md:py-10"
   >
-    <h2
-      class="text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold mb-2 text-center"
-    >
-      Votre Tableau de Bord Crypto Personnalisé : Suivez, Analysez, Optimisez
-    </h2>
+    <div class="mb-2 text-center">
+      <h2 class="text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold mb-5">
+        Votre Tableau de Bord Crypto Personnalisé : Suivez, Analysez, Optimisez
+      </h2>
+      <p>
+        Gardez une longueur d'avance dans le monde de la cryptomonnaie avec
+        notre tableau de bord personnalisé. Notre plateforme vous offre un
+        aperçu complet de vos investissements, avec des outils d'analyse et de
+        suivi en temps réel.
+      </p>
+    </div>
     <div class="w-full md:w-[80%] lg:w-[70%]">
       <img
         src="@/assets/img/dashboard.png"
