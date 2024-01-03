4fcba9ef2ae86b188bd38d5e5e6c2f48ec893309
Jolan <jolan.brizard@epitech.eu>
2024-01-03 23:28:34 +0100

    add role display and details page

---
+++ apps/bo/pages/admin/[id].vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["auth"],
+});
+
+const session = useSessionStore();
+const deleteUser = useDeleteUser();
+const loading = ref(false);
+const user = ref();
+
+const fetchAllUsers = async () => {};
+
+onMounted(async () => {
+  await fetchAllUsers();
+});
+
+const deleteUserById = async (id: number) => {
+  const response = await deleteUser(id);
+
+  if (!response.ok) {
+    return alert("Error happened while deleting user");
+  }
+
+  await fetchAllUsers();
+};
+</script>
+
+<template>
+  <section class="w-full">
+    <div class="max-w-5xl mx-auto mt-10">
+      <h1 class="text-2xl font-semibold border-b mb-2">${}</h1>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss"></style>
--- apps/bo/pages/admin/index.vue
+++ apps/bo/pages/admin/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { TrashIcon } from "@heroicons/vue/24/outline";
+import { TrashIcon, EyeIcon } from "@heroicons/vue/24/outline";
 import type { UIDataTable } from "@timeismoney/ui-components/types/ui-table";
 
 definePageMeta({
   middleware: ["auth"],
 });
+
+const session = useSessionStore();
 
 const getAllUsers = useGetAllUsers();
 const deleteUser = useDeleteUser();
@@ -14,6 +16,7 @@ const dataTable = ref<UIDataTable<any>>({
     { key: "firstname", label: "Firstname" },
     { key: "lastname", label: "Lastname" },
     { key: "nickname", label: "Username" },
+    { key: "role", label: "Role" },
     { key: "id", label: "Actions" },
   ],
   data: [],
@@ -69,11 +72,25 @@ const deleteUserById = async (id: number) => {
         <template #nickname="user">
           {{ user.data.nickname }}
         </template>
+        <template #role="user">
+          {{ user.data.role }}
+        </template>
 
         <template #id="user">
-          <button
+          <NuxtLink
             role="button"
-            id="delete"
+            class="btn btn-square btn-secondary no-underline mr-2"
+            id="detailsUser"
+            :to="'/admin/' + user.data.id"
+          >
+            <EyeIcon class="w-5 h-5 text-white" />
+          </NuxtLink>
+          <button
+            v-if="
+              user.data.id !== session.user?.id && user.data.role !== 'ADMIN'
+            "
+            role="button"
+            id="deleteUser"
             class="btn btn-square btn-error"
             @click="deleteUserById(user.data.id)"
           >
