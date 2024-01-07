ec870733629154713af3b59bae05b742508d84c0
kieran <kieran.boudouin@epitech.eu>
2024-01-08 00:37:17 +0100

    [STYLE] MADE UserUpdate Component responsive to dark/light mode

--- apps/front/components/UserUpdate.vue
+++ apps/front/components/UserUpdate.vue
@@ -1,18 +1,18 @@
 <template>
 <div class="flex items-center justify-center mb-10">
-    <div class="p-6 bg-white rounded shadow-md w-80">
+    <div class="p-6 bg-base-200 rounded-xl shadow-md w-80">
       <h2 class="text-2xl mb-4 font-semibold text-center">Your Profile</h2>
       <div class="mb-4">
-        <label class="block mb-2 text-sm font-medium text-gray-600">First Name:</label>
-        <input v-model="user.firstname" @change="updateUser('firstname', user.firstname)" class="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring focus:ring-blue-100" />
+        <label class="block mb-2 text-sm font-medium">First Name:</label>
+        <input v-model="user.firstname" @change="updateUser('firstname', user.firstname)" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-100" />
       </div>
       <div class="mb-4">
-        <label class="block mb-2 text-sm font-medium text-gray-600">Last Name:</label>
-        <input v-model="user.lastname" @change="updateUser('lastname', user.lastname)" class="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring focus:ring-blue-100" />
+        <label class="block mb-2 text-sm font-medium">Last Name:</label>
+        <input v-model="user.lastname" @change="updateUser('lastname', user.lastname)" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-100" />
       </div>
       <div class="mb-4">
-        <label class="block mb-2 text-sm font-medium text-gray-600">Nickname:</label>
-        <input v-model="user.nickname" @change="updateUser('nickname', user.nickname)" class="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring focus:ring-blue-100" />
+        <label class="block mb-2 text-sm font-medium ">Nickname:</label>
+        <input v-model="user.nickname" @change="updateUser('nickname', user.nickname)" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-100" />
       </div>
     </div>
   </div>
